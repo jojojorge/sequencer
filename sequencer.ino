112be467aeed8092ca00dfe4067d37535116dea3
Jojojorge <jorge.rodriguez@ardumusic.fr>
2014-11-26 10:49:20 +0100

    Bug interrupcion sequencer

--- sequencer.ino
+++ sequencer.ino
@@ -4,32 +4,44 @@
 //TODO: gestionar interrupciones externas
 // asignarles un estado
 
-
-const int LED1 = 12;                    //connect one LED to this pin (with appropriate current-limiting resistor of course)
-const int LED2 = 13;          //connect another LED to this pin (don't forget the resistor)
-const int LEDint = 11;
+const int LED1 = 10;                    //connect one LED to this pin (with appropriate current-limiting resistor of course)
+const int LED2 = 11; 
+const int LED3 = 12;                    //connect one LED to this pin (with appropriate current-limiting resistor of course)
+const int LED4 = 13;          //connect another LED to this pin (don't forget the resistor)
+const int LEDint = 9;
 const int BUTTON_PIN = 2;
 Timer t; 
 
 //Global variables for interruptions
-int volatile intState = LOW;
+int intState = LOW;
 
 //Global definitions
 int numberSteps = 4; //Numero de estados del sequencer
 int actualState = 0; //Define el numero del "tiempo" donde nos encontramos
 int actualMode = 0; //Define el modo: 1: play 2:record 3:stop 
+boolean LEDintState = LOW;
 
+//REGISTROS  DE NOTAS
+
+
+int nota1 = 0x40;
+int nota2 = 0x40;
+int nota3 = 0x40;
+int nota4 = 0x40;
 //instantiate the timer object
 
 void setup(void)
-{
+{ 
+    Serial.begin(115200);
     pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
     pinMode(LEDint, OUTPUT);
     pinMode(BUTTON_PIN, INPUT);
     digitalWrite(BUTTON_PIN, HIGH);
     t.every(500,changeState);
-    attachInterrupt(0, somethingIsHappening, FALLING);
+    attachInterrupt(0, somethingIsHappening, RISING);
 }
 
 void loop(void)
@@ -38,7 +50,7 @@ void loop(void)
     
     if(actualState==1)
     {
-      digitalWrite(LED2, LOW);
+      digitalWrite(LED4, LOW);
       digitalWrite(LED1, HIGH);
     }
     else if (actualState==2)
@@ -49,12 +61,12 @@ void loop(void)
     else if(actualState==3)
     {
       digitalWrite(LED2, LOW);
-      digitalWrite(LED1, HIGH);
+      digitalWrite(LED3, HIGH);
     }
     else if (actualState==4)
     {
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
     }
       
     
@@ -73,13 +85,14 @@ void changeState()
 }
 void somethingIsHappening()
 {
-  if (LEDintState)
-  {
-    digitalWrite(LEDint, LOW);
-  }
-  else
-  {
-    digitalWrite(LEDint, HIGH);
-  }
+  LEDintState = !LEDintState;
+  digitalWrite(LEDint, LEDintState);
+ 
+  
 }
 
+void noteOn(int cmd, int pitch, int velocity) {
+Serial.write(cmd);
+Serial.write(pitch);
+Serial.write(velocity);
+}
